b1daf128a6f2788d6a91af71c2912c60211af7f0
Jacob Pleiness <pleiness@umich.edu>
2019-12-08 12:54:43 -0500

    Block public access on imputation bucket

--- storage/s3/main.tf
+++ storage/s3/main.tf
@@ -185,6 +185,15 @@ resource "aws_s3_bucket" "imputation_server" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_public_access_block" "access_logs" {
+  bucket = aws_s3_bucket.imputation_server.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # ATTACH POLICY TO BUCKETS
 # ---------------------------------------------------------------------------------------------------------------------
