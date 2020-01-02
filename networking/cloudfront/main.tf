b86b15a715838fa0fc8dfaa19ce6e30a4a80bd62
Jacob Pleiness <pleiness@umich.edu>
2020-01-02 10:21:27 -0500

    Update Terraform version to 0.12.18

--- networking/cloudfront/main.tf
+++ networking/cloudfront/main.tf
@@ -20,7 +20,7 @@ terraform {
 
   # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
   # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-  required_version = "= 0.12.17"
+  required_version = "= 0.12.18"
 }
 
 data "aws_acm_certificate" "this" {
