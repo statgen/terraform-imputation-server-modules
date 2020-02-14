ed4dcdc2b232095699c8f549341a5a2782065a8c
Jacob Pleiness <pleiness@umich.edu>
2020-02-14 15:24:47 -0500

    update outputs

--- security/iam/outputs.tf
+++ security/iam/outputs.tf
@@ -1,17 +1,17 @@
-output "full_access_group_id" {
-  description = "The ID of the full-access group"
-  value       = aws_iam_group.full_access.id
-}
+# output "full_access_group_id" {
+#   description = "The ID of the full-access group"
+#   value       = aws_iam_group.full_access.id
+# }
 
-output "full_access_group_arn" {
-  description = "The ARN of the full-access group"
-  value       = aws_iam_group.full_access.arn
-}
+# output "full_access_group_arn" {
+#   description = "The ARN of the full-access group"
+#   value       = aws_iam_group.full_access.arn
+# }
 
-output "full_access_group_unique_id" {
-  description = "The unique ID assigned to the full-access group by AWS"
-  value       = aws_iam_group.full_access.unique_id
-}
+# output "full_access_group_unique_id" {
+#   description = "The unique ID assigned to the full-access group by AWS"
+#   value       = aws_iam_group.full_access.unique_id
+# }
 
 output "bastion_host_role_arn" {
   description = "The ARN of the bastion host role"
