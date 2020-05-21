4bb6a11d979ace03dfdd2b7a89b8f6818c0a58be
Jacob Pleiness <pleiness@umich.edu>
2020-05-21 11:20:16 -0400

    Add basic outputs to tgw

---
+++ networking/transit-gateway/outputs.tf
@@ -0,0 +1,9 @@
+output "transit_gateway_arn" {
+  description = "Transit Gateway Amazon Resource Name"
+  value       = module.transit-gateway.this_ec2_transit_gateway_arn
+}
+
+output "transit_gateway_id" {
+  description = "Transit Gateway ID"
+  value       = modlue.tansit-gateway.this_ec2_transit_gateway_id
+}
