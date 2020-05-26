a814027ee9ffeec48162673b41ccac6f081d53be
Jacob Pleiness <pleiness@umich.edu>
2020-05-26 14:09:38 -0400

    Fix output module reference

--- networking/transit-gateway/outputs.tf
+++ networking/transit-gateway/outputs.tf
@@ -1,9 +1,9 @@
 output "transit_gateway_arn" {
   description = "Transit Gateway Amazon Resource Name"
-  value       = module.transit-gateway.this_ec2_transit_gateway_arn
+  value       = module.transit_gateway.this_ec2_transit_gateway_arn
 }
 
 output "transit_gateway_id" {
   description = "Transit Gateway ID"
-  value       = module.transit-gateway.this_ec2_transit_gateway_id
+  value       = module.transit_gateway.this_ec2_transit_gateway_id
 }
