7360d3d30e5c8258b3f68368b0324c039a69cf93
Jacob Pleiness <pleiness@umich.edu>
2020-07-09 14:07:05 -0400

    Manage bootstrap files

---
+++ configuration/main.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  # The AWS region in which all resources will be created
+  region = var.aws_region
+
+  # Require a 2.x version of the AWS provider
+  version = "~> 2.6"
+
+  # Only these AWS Account IDs may be operated on
+  allowed_account_ids = var.aws_account_id
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# TERRAFORM STATE BLOCK
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt or via a backend.hcl file. See
+  # https://www.terraform.io/docs/backends/config.html#partial-configuration
+  backend "s3" {}
+
+  # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
+  # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
+  required_version = "= 0.12.28"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# UPLOAD FILES FOR IMPUTATION SERVER BOOTSTRAP
+# ---------------------------------------------------------------------------------------------------------------------
+
+locals {
+    data_path = "bucket-data"
+    bootstrap_path = "${local.data_path}/bootstrap.sh"
+    apps_file_path = "${local.data_path}/apps.yaml"
+}
+
+resource "aws_s3_bucket_object" "bootstrap_script" {
+    bucket = var.bucket_name
+    key = "bootstrap.sh"
+    source = local.bootstrap_path
+
+    etag = "${filemd5(local.bootstrap_path)}"
+
+    tags = var.tags
+}
+
+resource "aws_s3_bucket_object" "apps_file" {
+    bucket = var.bucket_name
+    key = "apps.yaml"
+    source = local.apps_file_path
+
+    etag = "${filemd5(local.apps_file_path)}"
+
+    tags = var.tags
+}
---
+++ configuration/variables.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {
+  description = "The AWS region in which all resources will be created"
+  type        = string
+  default     = null
+}
+
+variable "aws_account_id" {
+  description = "The ID of the AWS account in which all resources will be created"
+  type        = list(string)
+  default     = null
+}
+
+variable "bucket_name" {
+    description = "The name of the S3 bucket where objects will be uploaded"
+    type = string
+    default = null
+}
+
+variable "tags" {
+  description = "Tags to apply to module resources"
+  type        = map(string)
+  default = {
+    Terraform = true
+  }
+}
