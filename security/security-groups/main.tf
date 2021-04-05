c31967682d3f2cb80d0d8df744d79036a912cf49
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2021-04-05 10:53:53 -0400

    Update Terraform version
    - Terraform v0.14
    - Add required providers block

--- security/security-groups/main.tf
+++ security/security-groups/main.tf
@@ -1,14 +1,3 @@
-provider "aws" {
-  # The AWS region in which all resources will be created
-  region = var.aws_region
-
-  # Require a 2.x version of the AWS provider
-  version = "~> 3.2"
-
-  # Only these AWS Account IDs may be operated on
-  allowed_account_ids = var.aws_account_id
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # TERRAFORM STATE BLOCK
 # ---------------------------------------------------------------------------------------------------------------------
@@ -17,10 +6,14 @@ terraform {
   # The configuration for this backend will be filled in by Terragrunt or via a backend.hcl file. See
   # https://www.terraform.io/docs/backends/config.html#partial-configuration
   backend "s3" {}
+}
 
-  # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
-  # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-  required_version = ">= 0.13"
+provider "aws" {
+  # The AWS region in which all resources will be created
+  region = var.aws_region
+
+  # Only these AWS Account IDs may be operated on
+  allowed_account_ids = var.aws_account_id
 }
 
 # ---------------------------------------------------------------------------------------------------------------------
@@ -132,7 +125,7 @@ resource "aws_security_group_rule" "monitoring_hosts_emr_master_prometheus_ingre
   security_group_id = aws_security_group.monitoring_hosts.id
 }
 
-# Duplicate when using CIDR blocks rather than secuirty group IDs. Leaving in place for 
+# Duplicate when using CIDR blocks rather than security group IDs. Leaving in place for
 # future support of ref. security group ID across transit gateways
 # resource "aws_security_group_rule" "monitoring_hosts_emr_slave_prometheus_ingress" {
 #   type      = "ingress"
