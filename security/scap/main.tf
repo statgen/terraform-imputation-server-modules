a8ed9768db87bdf19699bdd545da821cb4790c6c
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2021-06-22 15:13:52 -0400

    Block public S3 access on SCAP S3 bucket

--- security/scap/main.tf
+++ security/scap/main.tf
@@ -78,6 +78,15 @@ resource "aws_s3_bucket" "this" {
   tags = var.tags
 }
 
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE SCAP init script
 # ---------------------------------------------------------------------------------------------------------------------
